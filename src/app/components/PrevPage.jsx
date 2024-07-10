395fb58bb7e5133b9fd4ad3ff79893f97c8b1687
Ravensthrope <95401791+Ravensthrope@users.noreply.github.com>
2024-07-10 15:39:11 +0530

    Completed UI of profile as well as edit profile

---
+++ src/app/components/PrevPage.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Link from "next/link";
+import { FaArrowLeft } from "react-icons/fa6";
+
+const PrevPage = ({ name, link }) => {
+  return (
+    <>
+      <Link
+        href={link}
+        className="w-full p-4 absolute top-0 left-0 flex flex-row gap-x-2"
+      >
+        <FaArrowLeft className="text-black sm:text-3xl text-4xl" />
+        <span className="text-sm sm:text-base text-gray-600 mt-1 font-semibold">
+          {name}
+        </span>
+      </Link>
+    </>
+  );
+};
+
+export default PrevPage;
---
+++ src/app/components/index.js
@@ -0,0 +1,6 @@
+import ProfilePic from "./ProfilePic";
+import More from "./More";
+import PrevPage from "./PrevPage";
+import ChangeProfile from "./ChangeProfile";
+
+export { ProfilePic, More, PrevPage, ChangeProfile };
