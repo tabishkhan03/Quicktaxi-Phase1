46160dba7fda1e38017f1546f236fc8e46f3bcf9
manvithshetty507 <manvithshetty18@gmail.com>
2024-07-13 21:29:32 +0530

    added the driver fetch api

--- src/app/api/drivers/alldrivers/route.js
+++ src/app/api/drivers/alldrivers/route.js
@@ -5,9 +5,14 @@ const prisma = new PrismaClient();
 export async function GET(request) {
 
   try {
-    const alldrivers = await prisma.driver.findFirst();
+    const alldrivers = await prisma.driver.findMany({
+      include:{
+        taxis:true
+      }
+    });
     return new Response(JSON.stringify(alldrivers), { status: 200 });
   } catch (error) {
+    console.log(error)
     return new Response(JSON.stringify({ error: 'Unable to fetch drivers ' }), { status: 500 });
   }
 }
--- src/components/carcard/Cardata.jsx
+++ src/components/carcard/Cardata.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useContext } from 'react';
-import Data from '../../app/data.js';
+import React, { useState, useContext, useEffect } from 'react';
+// import Data from '../../app/data.js';
 import { SlArrowUp, SlArrowDown } from "react-icons/sl";
 import { AppContext } from '../../../context/AppContext'; // Correct import for AppContext
 
@@ -7,6 +7,7 @@ const Cardata = () => {
   const [menu, setMenu] = useState(true);
   const { state } = useContext(AppContext); // Destructuring state from AppContext
   const { sourceLocation, destinationLocation, direction } = state; // Destructuring sourceLocation, destinationLocation, direction from state
+  const [driverData, setDriverData] = useState([]);
 
   const getCost = (charge) => {
     if (direction && direction.routes && direction.routes.length > 0) {
@@ -16,6 +17,26 @@ const Cardata = () => {
       return '00.00'; // Default value when direction data is not available
     }
   };
+
+  const fetchData = async () => {
+    const res = await fetch('http://localhost:3000/api/drivers/alldrivers');
+    const data = await res.json();
+    console.log("data", data);
+    const finalData = data.map((driver) => {
+      return {
+        driver:driver.name,
+        name:driver.taxis[0].model,
+        number:driver.taxis[0].number,
+        charge:driver.taxis[0].charge,
+        distance:driver.taxis[0].distance,
+      }
+    })
+    setDriverData(finalData);
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
 
   return (
     <div className="w-full">
@@ -34,24 +55,27 @@ const Cardata = () => {
                   <SlArrowDown size={35} />
                 </button>
               </div>
-              {Data.map((cars, id) => (
-                <div
-                  className="flex justify-between px-2 border-1.5px border-gray-200 py-2 hover:bg-yellow-300"
-                  key={id}
-                >
-                  <div className="flex items-center gap-3">
-                    <div className="w-9 text-center text-xl">{cars.id}</div>
-                    <div>
-                      <p className="text-lg font-medium">{cars.name}</p>
-                      <p className="text-green-400">{cars.distance}</p>
+              <div className="max-h-64 overflow-y-scroll" style={{ scrollbarWidth: 'none', msOverflowStyle: 'none' }}>
+                {driverData.map((cars, id) => (
+                  <div
+                    className="flex justify-between px-2 border-1.5px border-gray-200 py-2 hover:bg-yellow-300"
+                    key={id}
+                    style={{ backgroundColor: id % 2 === 0 ? '#f0f0f0' : 'transparent' }} // Example of inline style
+                  >
+                    <div className="flex items-center gap-3">
+                      <div className="w-9 text-center text-xl">{id}</div>
+                      <div>
+                        <p className="text-lg font-medium">{cars.name}</p>
+                        <p className="text-green-400">{cars.distance}</p>
+                      </div>
+                    </div>
+                    <div className='flex flex-col items-center mx-2'>
+                      <p className="text-xl font-medium">{cars.number}</p>
+                      <p className="text-green-400">rs.{getCost(cars.charge)}</p>
                     </div>
                   </div>
-                  <div>
-                    <p className="text-xl font-medium">{cars.number}</p>
-                    <p className="text-green-400">rs.{getCost(cars.charge)}</p>
-                  </div>
-                </div>
-              ))}
+                ))}
+              </div>
             </div>
           )}
         </div>
