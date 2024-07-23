ae95098dbc62a67078ab64827d1db6a753daa854
Ravensthrope <95401791+Ravensthrope@users.noreply.github.com>
2024-07-23 20:08:34 +0530

    Some work on the second page

--- src/app/components/PrevPage.jsx
+++ src/app/components/PrevPage.jsx
@@ -7,7 +7,7 @@ const PrevPage = ({ name, link }) => {
     <>
       <Link
         href={link}
-        className="w-full p-4 absolute top-0 left-0 flex flex-row gap-x-2"
+        className="w-fit p-4 absolute top-0 left-0 flex flex-row gap-x-2"
       >
         <FaArrowLeft className="text-black sm:text-3xl text-4xl" />
         <span className="text-sm sm:text-base text-gray-600 mt-1 font-semibold">
