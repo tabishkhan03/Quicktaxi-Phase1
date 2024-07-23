15d028829d905b675b9a8c77cea2132cea2084de
Ravensthrope <95401791+Ravensthrope@users.noreply.github.com>
2024-07-23 18:26:44 +0530

    made it more responsive

--- src/app/registration-driver/page.jsx
+++ src/app/registration-driver/page.jsx
@@ -6,15 +6,17 @@ const page = () => {
   return (
     <>
       <div className="flex items-center justify-center min-h-screen bg-black">
-        <div className="w-full sm:w-[71.6mm] min-h-screen sm:min-h-[147.6mm] max-h-screen sm:max-h-[147.6mm] bg-[rgb(var(--foreground-rgb))] text-black border border-gray-300 bg-white rounded-lg shadow-lg overflow-hidden relative flex flex-col items-center justify-between p-8 gap-y-4">
-          <h1 className="uppercase text-black sm:text-4xl text-3xl font-extrabold">
-            QUICKTAXI
-          </h1>
-          <div className="h-0.5 w-full bg-black rounded-full" />
+        <div className="w-full sm:w-[71.6mm] min-h-screen sm:min-h-[147.6mm] max-h-screen sm:max-h-[147.6mm] bg-[rgb(var(--foreground-rgb))] text-black border border-gray-300 bg-white rounded-lg shadow-lg overflow-hidden relative flex flex-col items-center justify-between sm:p-8 p-6 ">
+          <div className="flex flex-col gap-y-4">
+            <h1 className="uppercase text-black sm:text-4xl text-3xl font-extrabold">
+              QUICKTAXI
+            </h1>
+            <div className="h-0.5 w-full bg-black rounded-full" />
+          </div>
           <img
             src="/driver/welcome-driver.PNG"
             alt="welcome-driver"
-            className="w-full h-44 bg-transparent bg-none"
+            className="sm:w-full sm:h-44 w-fit bg-transparent bg-none"
           />
           <div className="flex flex-col gap-0">
             <span className="sm:text-xs text-center font-bold">
@@ -28,7 +30,7 @@ const page = () => {
           <div className="w-full flex justify-end">
             <Link
               href="/"
-              className="flex flex-row gap-2 bg-yellow-400 text-black hover:bg-yellow-500 rounded-3xl w-3/4 h-8 text-center py-1 px-2 justify-between"
+              className="flex flex-row gap-2 bg-yellow-400 text-black hover:bg-yellow-500 rounded-3xl sm:w-3/4 w-fit h-8 text-center py-1 px-2 justify-between"
             >
               <span>Get Started</span>
               <FaArrowRight className="text-xl m-0.5" />
