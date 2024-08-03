c771ca108ef14ec4a2030ce5bc82661a49ef2295
tabishkhan03 <tabish25u@gmail.com>
2024-08-04 02:06:12 +0530

    commit

--- src/app/api/customers/cancelride/route.js
+++ src/app/api/customers/cancelride/route.js
@@ -1,33 +1,3 @@
-// import { PrismaClient } from "@prisma/client";
-
-// const prisma = new PrismaClient();
-
-// export default async function handler(req, res) {
-//   if (req.method !== "PUT") {
-//     return res.status(405).json({ error: "Method not allowed" });
-//   }
-
-//   const { trip_id } = req.body;
-
-//   if (!trip_id) {
-//     return res.status(400).json({ error: "Missing required fields" });
-//   }
-
-//   try {
-//     const updatedTrip = await prisma.trip.update({
-//       where: { trip_id: parseInt(trip_id) },
-//       data: { status: "canceled" },
-//     });
-
-//     res
-//       .status(200)
-//       .json({ message: "Trip canceled successfully", trip: updatedTrip });
-//   } catch (error) {
-//     res
-//       .status(500)
-//       .json({ error: "Error canceling trip", details: error.message });
-//   }
-// }
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
@@ -44,7 +14,7 @@ export async function PUT(request) {
     }
 
     const updatedTrip = await prisma.trip.update({
-      where: { trip_id: parseInt(trip_id) },
+      where: { trip_id: trip_id },
       data: { status: "canceled" },
     });
 
--- src/app/api/trips/complete-trip/route.js
+++ src/app/api/trips/complete-trip/route.js
@@ -21,7 +21,7 @@ export async function PUT(request) {
 
 
     const updatedTrip = await prisma.trip.update({
-      where: { trip_id: parseInt(trip_id) },
+      where: { trip_id: trip_id },
       data: { end_time, status: 'completed' },
     });
 
