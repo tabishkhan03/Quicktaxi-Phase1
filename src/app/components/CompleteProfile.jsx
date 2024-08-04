8dae719beec1faa591ec8a9a5aad2b71ae46bc9c
tabishkhan03 <tabish25u@gmail.com>
2024-08-04 11:08:52 +0530

    changed the path

--- src/app/components/CompleteProfile.jsx
+++ src/app/components/CompleteProfile.jsx
@@ -1,3 +1,4 @@
+"use client"
 import React, { useState } from "react";
 import { IoChevronForward, IoPerson, IoBusinessOutline } from "react-icons/io5";
 import ButtonWithArrow from "./ButtonWithArrow";
